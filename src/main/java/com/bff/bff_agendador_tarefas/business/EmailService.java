46efa4c95ea545e73680859e0378333a61af9173
Michael Leivas <macleivas@hotmail.com>
2025-03-03 17:13:40 -0300

    adiiconados client email

---
+++ src/main/java/com/bff/bff_agendador_tarefas/business/EmailService.java
@@ -0,0 +1,18 @@
+package com.bff.bff_agendador_tarefas.business;
+
+import com.bff.bff_agendador_tarefas.business.dto.out.TarefasDTOResponse;
+import com.bff.bff_agendador_tarefas.infrastructure.client.EmailClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final EmailClient emailClient;
+
+    public void enviaremail(TarefasDTOResponse dto){
+        emailClient.enviarEmail(dto);
+    }
+
+}
